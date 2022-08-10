497a95829df9a59c949e9cb2ddd1380e36547643
khanhhoang197 <khanhhoang197.wbs@gmail.com>
2022-08-10 11:03:52 +0700

    Dien tich hinh chu nhat

---
+++ IntroductionToJava/src/Bai3_SD_Toan_Tu/Shcn.java
@@ -0,0 +1,20 @@
+package Bai3_SD_Toan_Tu;
+import java.util.Scanner;
+public class Shcn {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        float width;
+        float leng;
+        float S;
+
+        System.out.print("Nhập chiều rộng của hình chữ nhật: ");
+        width = scanner.nextFloat();
+
+        System.out.print("Nhập chiều rộng của hình chữ nhật: ");
+        leng = scanner.nextFloat();
+
+        S = width * leng;
+
+        System.out.print("Diện tích hình chữ nhật là: " + S);
+    }
+}
