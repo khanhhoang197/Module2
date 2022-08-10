9b6420aefb360d6f769d1f7ee849d1e61c90c7d3
khanhhoang197 <khanhhoang197.wbs@gmail.com>
2022-08-10 10:00:42 +0700

    system-time

---
+++ System-time/src/Time.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class Time {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Thời gian hiện tại là: "+ now);
+    }
+}
