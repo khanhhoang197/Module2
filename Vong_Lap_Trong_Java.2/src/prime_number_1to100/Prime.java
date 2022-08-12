9d5705b27a8617044babf1d7be0b749f76017f3c
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-08-12 14:07:01 -0700

    prime be hon 100

---
+++ Vong_Lap_Trong_Java.2/src/prime_number_1to100/Prime.java
@@ -0,0 +1,25 @@
+package prime_number_1to100;
+
+public class Prime {
+    public static void main(String[] args) {
+        System.out.println("Các số nguyên tố nhỏ hơn 100 là: ");
+        for (int i = 0; i <= 100; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
