a16ccccdd5a7da1ae412e783784e023be18d3a50
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-08-12 17:31:16 -0700

    temperature changle

---
+++ Array_3/src/temperature_change/Change.java
@@ -0,0 +1,47 @@
+package temperature_change;
+
+import java.util.Scanner;
+
+public class Change {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double fahrenheit;
+        double celsius;
+        int change = -1;
+        System.out.println("Menu");
+        do {
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit ");
+            System.out.print("Select: ");
+            change = scanner.nextInt();
+
+            switch (change) {
+                case 1:
+                    System.out.println("Enter fahrenheit: ");
+                    fahrenheit = scanner.nextDouble();
+                    System.out.println("Fahrenheit to Celsius: " + fahrenheitToCelsius(fahrenheit));
+                    break;
+                case 2:
+                    System.out.println("Enter Celsius: ");
+                    celsius = scanner.nextDouble();
+                    System.out.println("Celsius to Fahrenheit: " + celsiusToFahrenheit(celsius));
+                    break;
+                case 0:
+                    System.exit(0);
+
+            }
+        }while (change != 0);
+
+    }
+
+    public static double celsiusToFahrenheit(double celsius){
+        double fahrenheit = (9.0 / 5) * celsius + 32;
+        return fahrenheit;
+    }
+    public static double fahrenheitToCelsius(double fahrenheit){
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        return celsius;
+    }
+}
+
