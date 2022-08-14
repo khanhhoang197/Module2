e18d4f4c934ae3c8ac638292937dcea2e28df2c1
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-08-14 14:33:23 -0700

    minimum value

---
+++ Array_3/src/find_minimum/Min.java
@@ -0,0 +1,20 @@
+package find_minimum;
+
+public class Min {
+    public static void main(String[] args) {
+        int[] arr = {18, 2, 5, 6, 74, 1, 9, 10, 42};
+        int index = minValue(arr);
+        System.out.printf("Giá trị nhỏ nhất trong mảng là: %d", arr[index]);
+    }
+
+    public static int minValue(int[] array){
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+
+        }
+        return index;
+    }
+}
