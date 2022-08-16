22919cd23f19e7690f472332a601c55a2dbfd600
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-08-16 17:23:08 +0700

    cai dat thuat toan tim kiem nhi phan không su dung de quy

--- Sort_algorithm_14/src/bubble_sort_by_step/BubbleSortByStep.java
+++ Sort_algorithm_14/src/bubble_sort_by_step/BubbleSortByStep.java
@@ -50,21 +50,18 @@ public static void bubbleSortByStep(int[] list) {
         needNextPass = false;
         for (int i = 0; i < list.length - k; i++) {
             if (list[i] > list[i + 1]) {
-                /* Swap list[i] with list[i + 1] */
                 System.out.print("Swap " + list[i] + " with " + list[i + 1]);
                 int temp = list[i];
                 list[i] = list[i + 1];
                 list[i + 1] = temp;
 
-                needNextPass = true; /* Next pass still needed */
+                needNextPass = true;
             }
         }
-        /* Array may be sorted and next pass not needed */
         if (needNextPass == false) {
             System.out.println("Array may be sorted and next pass not needed");
             break;
         }
-        /* Show the list after sort */
         System.out.print("List after the  " + k + "' sort: ");
         for (int j = 0; j < list.length; j++) {
             System.out.print(list[j] + "\t");
