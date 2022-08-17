19c0c4556b71c28ec25314c724adf23c55053f08
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-08-17 15:27:53 +0700

    stop watch

---
+++ OOP_4/src/stop_watch/MainStopWatch.java
@@ -0,0 +1,32 @@
+package stop_watch;
+
+import java.time.LocalTime;
+
+public class MainStopWatch {
+    public static int[] selectionSort(int... a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[i]) {
+                    int tempt = a[i];
+                    a[i] = a[j];
+                    a[j] = tempt;
+                }
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[100000];
+        for (int i = 0; i < 100000; i++) {
+            a[i] = (int) (Math.random() * 1000);
+        }
+        LocalTime start = LocalTime.now();
+        System.out.print("Start Time: " + start);
+        a = selectionSort(a);
+        LocalTime end = LocalTime.now();
+        System.out.print("\nEnd Time: " + end);
+        StopWatch swt = new StopWatch(start, end);
+        System.out.println("\nTime: " + swt.getElapsedTime() + " milisecond");
+    }
+}
