7f977a9cbade758d2c1735b097e60c59fee48264
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-08-30 14:05:15 +0700

    queue

---
+++ DSA_Stack_Queue_11/src/implemention_queue/LinkedListQueueClient.java
@@ -0,0 +1,16 @@
+package implemention_queue;
+
+public class LinkedListQueueClient {
+    public static void main(String[] args) {
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.printf("Dequeue items is:", queue.dequeue().key);
+
+    }
+}
