0b21b0fa469930b84d10beaaa966a09a66abb5b6
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-09-05 10:41:43 +0700

    tinh tong cac so trong file

---
+++ Text_File_16/src/sum_file_text/ReadFileExample.java
@@ -0,0 +1,39 @@
+package sum_file_text;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class ReadFileExample {
+    public void readFileText(String filePath) {
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            int sum = 0;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                sum += Integer.parseInt(line);
+            }
+            br.close();
+            System.out.println("Tổng = " + sum);
+        } catch (
+                Exception e) {
+            // TH file không tồn tại hoặc nội dung file có lỗi thì sẽ hiển thị thông báo lỗi.
+            System.err.println("Fie không tồn tại or nội dung có lỗi!");
+        }
+    }
+    public static void main (String[]args){
+        System.out.print("Nhập đường dẫn file: ");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+
+        ReadFileExample readfileEx = new ReadFileExample();
+        readfileEx.readFileText(path);
+    }
+}
