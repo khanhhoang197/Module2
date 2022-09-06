a6e7996aee002e5b93846079787a04a99ca74745
khanhhhhhh <khanhhoang197.wbs@gmail.com>
2022-09-06 14:14:07 +0700

    racing cars

---
+++ Threading_18/src/racing_cars/Car.java
@@ -0,0 +1,44 @@
+package racing_cars;
+
+import java.util.Random;
+
+import static racing_cars.Main.DISTANCE;
+import static racing_cars.Main.STEP;
+
+public class Car implements Runnable{
+    private String name;
+    public Car(String name){
+        this.name = name;
+    }
+    @Override
+    public void run(){
+        int runDistance = 0;
+        long startTime = System.currentTimeMillis();
+        while (runDistance < DISTANCE){
+            try {
+                int speed = (new Random()).nextInt(20);
+                runDistance += speed;
+                String log = "|";
+                int percentTravel = (runDistance * 100) / DISTANCE;
+                for (int i = 0; i <DISTANCE; i+=STEP){
+                    if (percentTravel >= i + STEP){
+                        log += "+";
+                    }else if (percentTravel >= i && percentTravel< i + STEP){
+                        log += "o";
+                    }else {
+                        log += "-";
+                    }
+                }
+                log += "|";
+                System.out.printf("\nCar %s : %s %d Km",this.name,log,Math.min(DISTANCE,runDistance));
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException e){
+                System.out.printf("\nCar %s broken...",this.name);
+                break;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.printf("\nCar %s Finish in %d s",this.name,(endTime - startTime)/100);
+    }
+}
